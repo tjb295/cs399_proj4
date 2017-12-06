b019b2a1259f31ea52867b28a0bcd1574073c886
Thomas Back <tjb295@nau.edu>
2017-12-06 15:27:47 -0700

    Adding first push of repo

---
+++ Assets/scripts/player_move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float movespeed;
+    public bool moveright;
+    public bool moveleft;
+    public bool jump;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+
+        }
+        if (moveright)
+        {
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+        }
+        if (moveleft)
+        {
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);
+        }
+
+        if (jump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, movespeed);
+        }
+    }
+}
---
+++ Assets/scripts/touch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+    private player_move player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<player_move>();
+    }
+
+    public void LeftArrow()
+    {
+        player.moveright = false;
+        player.moveleft = true;
+    }
+    public void RightArrow()
+    {
+        player.moveright = true;
+        player.moveleft = false;
+    }
+    public void ReleaseLeftArrow()
+    {
+        player.moveleft = false;
+    }
+    public void ReleaseRightArrow()
+    {
+        player.moveright = false;
+
+    }
+
+    public void JumpKey()
+    {
+        player.jump = true;
+    }
+
+    public void ReleaseJumpKey()
+    {
+        player.jump = false;
+    }
+
+}
